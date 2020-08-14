b3798eab7172fd476a186c29d265184cfd089884
Juan Carlos Sánchez Gutiérrez <jcsagu00@gmail.com>
2020-08-13 23:00:51 -0500

    Initial Commit

---
+++ YoYo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  YoYo
+//
+//  Created by Juan Carlos SG on 13/08/20.
+//  Copyright © 2020 Panteras UP Coding. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
